17fe3ca36661079fcd434677d5320c29e12cea30
João Expedito <joaoararipe256@gmail.com>
2026-01-12 22:33:27 -0300

    Adicionando paredes e sistema de colisão com o próprio corpo da cobra

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 from time import sleep
 from snake import Snake
 from point import Point
@@ -9,6 +9,16 @@ screen.setup(600, 600)
 screen.title('SNAKE GAME')
 screen.bgcolor('black')
 screen.tracer(0)
+
+wall = Turtle()
+wall.color("white")
+wall.pensize(10)
+wall.hideturtle()
+wall.teleport(-300, 300)
+
+for a in range(4):
+    wall.forward(600)
+    wall.right(90)
 
 snake = Snake()
 
@@ -26,20 +36,26 @@ game_is_on = True
 
 while game_is_on:
     screen.update()
-    sleep(0.1)    
+    sleep(0.2)    
 
     snake.move()
 
-    snake_head_position = snake.segments[0].pos()
-
+    # se a cobra pegar um ponto, cria um novo ponto na tela e aumenta o score
     if snake.head.distance(point) < 15:
+        snake.extend()
         point.create_new_point()
         scoreboard.increase_score()
 
+    # acaba o jogo quando a cobra colide com os limites da tela
     if (snake.head.xcor() > 280 or snake.head.xcor() < -280 or 
         snake.head.ycor() > 280 or snake.head.ycor() < -280):
         scoreboard.game_over()
         game_is_on = False
+
+    for a in range(1, len(snake.segments)):
+        if snake.segments[0].distance(snake.segments[a]) < 10:
+            scoreboard.game_over()
+            game_is_on = False
 
 
 screen.exitonclick()
